52cec5db6b287fd26739b005f7985f63ea2c81eb
Xayah <zds1249475336@gmail.com>
2024-01-09 00:35:55 +0800

    Revert "Fix HTTP 207 didn't handled properly."
    This reverts commit 2365354c456712fdc89f313825bb170cc052dfa5.

--- libsardine/src/main/java/com/xayah/libsardine/impl/OkHttpSardine.java
+++ libsardine/src/main/java/com/xayah/libsardine/impl/OkHttpSardine.java
@@ -404,7 +404,8 @@ public class OkHttpSardine implements Sardine {
     public boolean exists(String url) throws IOException {
         Request request = new Request.Builder()
                 .url(url)
-                .method("HEAD", null)
+                .header("Depth", "0")
+                .method("PROPFIND", null)
                 .build();
 
         return execute(request, new ExistsResponseHandler());
